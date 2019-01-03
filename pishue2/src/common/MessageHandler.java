11eef0aa9785efa3f7db5958e0c85e7dd7fe0ab3
veradi <46264754+veradi@users.noreply.github.com>
2019-01-03 09:40:36 +0100

    Update MessageHandler.java

--- pishue2/src/common/MessageHandler.java
+++ pishue2/src/common/MessageHandler.java
@@ -1,29 +1,14 @@
 package common;
 
-import javafx.application.Platform;
-
 public class MessageHandler {
 
     private String cmd ="";
     private String msg ="";
-    private String name ="";
-
-    public MessageHandler(String cmd, String msg){
-        this.cmd = cmd;
-        this.msg = msg;
-    }
-
-    public MessageHandler(String cmd, String name, String msg){
-        this.cmd = cmd;
-        this.name = name;
-        this.msg = msg;
-    }
 
     public MessageHandler(String msg){
         String[] msgSplit = msg.split(":");
         this.cmd = msgSplit[0];
         this.msg = msgSplit[1];
-        System.out.println(cmd+" "+this.msg);
         if(msgSplit.length>2){
             for(int i = 2; i < msgSplit.length; i++){
                 this.msg = this.msg+":"+msgSplit[i];
@@ -37,17 +22,6 @@ public class MessageHandler {
 
     public String getMsg() {
         return msg;
-    }
-
-    public String constructMessage(){
-        String message = "";
-        if(!name.equals("")){
-            message = cmd+":"+name+":"+msg;
-        }
-        else{
-            message = cmd+":"+msg;
-        }
-        return message;
     }
 
     public String[] getUsers(){
